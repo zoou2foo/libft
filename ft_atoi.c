1ea969e2c9e8f885390d8564c6dc9694db70314d
zoou2foo <99362246+zoou2foo@users.noreply.github.com>
2022-04-25 16:20:55 -0600

    Commentaires pour reviser

--- ft_atoi.c
+++ ft_atoi.c
@@ -11,7 +11,19 @@
 /* ************************************************************************** */
 
 #include"libft.h"
-#include<stdio.h>
+// argument to int. J'ai besoin de 3 variables. Une pour contenirr le resultat
+// final, une pour aider a gerer les negatifs et un index. 1. Pendant qu'a 
+// l'index i dans la str est plus grand ou egal a 9 ET plus petit ou egal a 13
+//  OU l'index i dans str est vide, on incremente i (on avance dans la chaine)
+// 2. le 1er if gere s'il y a 2 signes consecutifs (+-) de sortir du programme
+// 3. le 2e if, cela gere les nombres positifs ou negatifs. Si positif, on
+// incremente (avance dans la chaine). Si negatif, sign devient -1. 3. 2e 
+// boucle while si l'index i de la chaine est plus grand ou egal a 0 ET plus 
+// petit ou egal a 9. Pour gerer les nombres entiers. Operation de la magie:
+// La variable res est egal a (res * 10), car on travaille en base de 10.
+// plus la position de l'index dans la chaine moins 0 ou 48 (ascii); et 
+// incremente. Quand while n'est plus vrai, on sort. Puis, retourne res * 
+// sign pour le resultat final.
 
 int	ft_atoi(const char *str)
 {
