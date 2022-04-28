7db7cab38af39de2b6603e96723ac4fec795f773
zoou2foo <99362246+zoou2foo@users.noreply.github.com>
2022-04-28 16:37:12 -0700

    Commentaires revision

--- ft_memset.c
+++ ft_memset.c
@@ -10,6 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+// La fonction remplie n bloc d'une chaine avec un caractere (char). Il faut 
+// alors cast ce qui se trouve au pointeur b en char. Une macro de valeur 10.
+// Len est le nombre de bytes a 'set'. Quand i est plus petit que len, on cast
+// b ou il se trouve selon l'index et il egale maintenant c. Puis, on incremente
+// jusqu'a ce que i egale a len.
 #include"libft.h"
 
 void	*ft_memset(void *b, int c, size_t len)
