4e55edf4f97a124d7ed44fb505cf2fbbf1bf6f1c
zoou2foo <99362246+zoou2foo@users.noreply.github.com>
2022-04-27 21:56:18 -0700

    Commentaires revisions

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -10,6 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+// fonction qui compare la memoire. Elle compare les bytes de la chaine s1 aux
+// bytes de la chaine s2. Le n represente le nombre de byte a comparer. Comme
+// ce sont des chaines "void" il faut les caster en char. Si n est zero, 
+// retourner 0 pour sortir du programme. Besoin d'un index pour se balader.
+// Tant que l'index est plus petit que le nombre de byte a comparer moins 1
+// pour ne pas inclure '\0'. Peu importe, la fonction retourne la difference
+// entre s1 et s2. Que ce soit positif ou negatif.
 #include"libft.h"
 #include<string.h>
 
