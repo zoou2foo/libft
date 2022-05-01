3ccba8a59a508ccfa796316a5c185c443213cfca
zoou2foo <99362246+zoou2foo@users.noreply.github.com>
2022-04-30 17:14:40 -0700

    Commentaires revision

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -10,6 +10,15 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+// fonction qui enleve les caracteres de 'set' dans la chaine. S'il n'y a rien
+// dans la chaine OU dans set, retourne NULL. Pendant, qu'il y a de quoi dans
+// s1 ET que dans la recherche de set dans s1 n'est pas NULL (a la fin), on
+// incremente (on continue de se balader. Variable end qui est egale a la 
+// longueur de la chaine. Pendant qu'il y a un nombre dans 'end' et que la
+// recherche de 'set' dans 's1 a partir de la fin' n'est pas NULL (n'est pas
+// a la fin), on decremente la variable fin (car on commence a la fin ici.) Puis,
+// on retourne la nouvelle string cree sans le set (sans oublier d'ajout '\0'.
+
 #include"libft.h"
 
 char	*ft_strtrim(char const *s1, char const *set)
