f3bcabdd283d14d4fb877570d4cd51af69aa9f95
zoou2foo <99362246+zoou2foo@users.noreply.github.com>
2022-04-30 17:53:12 -0700

    Commentaires revisioln

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -10,6 +10,15 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+// Fonction qui copie et concatenate des chaines. 2 compteurs pour faciliter
+// le deplacement dans source et destination. 2 variables: une pour la longueur
+// de src et une pour la longueur de dst. i devient la longueur de dst. Si dstlen
+// est plus petit que la taille de dst moins 1 (ne va pas aller au-dela de '\0')
+// ET que dstsize est plus grand que 0 --> pendant qu'il y a de quoi dans 
+// source ET que dstlen plus l'index j est plus petit que dstsize - 1 (pour ne pas
+// aller au-dela '\0'), copie et on ajoute un '\0' a la fin de dest. Si dstlen
+// est plus grand ou egal dstsize, dslen devient egal a dstsize. Puis, on retourne
+// dstlen + srclen.
 #include"libft.h"
 
 size_t	ft_strlcat(char *dst, char *src, size_t dstsize)
